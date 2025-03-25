85cd6754515c717d95a9585926da6ab453235d71
verdx <berkut589243@gmail.com>
2025-03-25 13:58:37 +0700

    create project

---
+++ AppLearnLanguage/AppLearnLanguage/AppLearnLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLearnLanguageApp.swift
+//  AppLearnLanguage
+//
+//  Created by Виталий Багаутдинов on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppLearnLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
